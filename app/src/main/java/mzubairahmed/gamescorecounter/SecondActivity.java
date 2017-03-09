8f71d64bdc12074ecbea5366774fb87b3d14cf5f
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-09 12:52:11 +0530

    change of design clean up

--- app/src/main/java/mzubairahmed/gamescorecounter/SecondActivity.java
+++ app/src/main/java/mzubairahmed/gamescorecounter/SecondActivity.java
@@ -1,33 +1,21 @@
 package mzubairahmed.gamescorecounter;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.coordinator_main);
+        setContentView(R.layout.coordinator_second);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TextView textView = (TextView) findViewById(R.id.gotos);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
     }
-
 
 
     @Override
